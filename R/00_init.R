c5d7a937ce7818d0567dc2799f95ed03b8ba1580
Stathis Gennatas <gennatas@gmail.com>
2026-05-25 17:44:57 -0700

    get get_decom_name

--- R/00_init.R
+++ R/00_init.R
@@ -1,3 +1,4 @@
 # rtemis internal functions
 get_alg_name <- getFromNamespace("get_alg_name", "rtemis")
+get_decom_name <- getFromNamespace("get_decom_name", "rtemis")
 msg <- getFromNamespace("msg", "rtemis")
